f0222fd5fdbd3d411a017ff4c61a234ab83d9804
Rhett Harrison <reharri7@asu.edu>
2025-06-08 15:00:47 -0700

    Generate Angular service layer with OpenAPI Generator for API integration

---
+++ client/linup-logic/src/app/services/generated/model/models.ts
@@ -0,0 +1,6 @@
+export * from './apiAuthLoginPost200Response';
+export * from './apiAuthLoginPost200ResponseUser';
+export * from './apiAuthLoginPost401Response';
+export * from './apiAuthLoginPostRequest';
+export * from './apiUsersGet200ResponseInner';
+export * from './apiUsersPostRequest';
